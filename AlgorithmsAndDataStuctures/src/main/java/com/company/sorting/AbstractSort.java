b31473bbfef302bf3c0bdff9082f1320e032382a
Mateusz <mateusz.ostro@o2.pl>
2021-12-02 20:45:31 +0100

    updated AbstractSort

--- AlgorithmsAndDataStuctures/src/main/java/com/company/sorting/AbstractSort.java
+++ AlgorithmsAndDataStuctures/src/main/java/com/company/sorting/AbstractSort.java
@@ -1,10 +1,31 @@
 package com.company.sorting;
 
-public abstract class AbstractSort {
+import lombok.Getter;
+import lombok.Setter;
 
+/**
+ * Sort class template.
+ * @author mrcombajn
+ * @since 02.12.2021
+ * @version 0.0.1
+ */
+public abstract class AbstractSort implements Sort {
+
+    @Getter
+    @Setter
     protected String sortName;
 
+    @Getter
+    @Setter
     protected String sortDescription;
 
-
+    /**
+     * Initializes a new instance of class that inherits AbstractSort.
+     * @param sortName Sorting name.
+     * @param sortDescription Sorting description.
+     */
+    public AbstractSort(String sortName, String sortDescription) {
+        this.sortName = sortName;
+        this.sortDescription = sortDescription;
+    }
 }
